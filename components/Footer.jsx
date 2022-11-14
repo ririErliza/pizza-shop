4dc2f5e76f7878be447830bda4723e3884b48018
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-14 11:31:01 +0100

    footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import css from '../styles/Footer.module.css'
+import {UilFacebook, UilWhatsapp, UilInstagram} from '@iconscout/react-unicons'
+import Image from 'next/image'
+import Logo from '../assets/Logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={css.container}>
+      <span>ALL RIGHT RESERVED</span>
+
+      <div className={css.social}>
+        <UilFacebook size={45}/>
+        <UilWhatsapp size={45}/>
+        <UilInstagram size={45}/>
+      </div>
+
+      <div className={css.logo}>
+        <Image src={Logo} alt="" width={50} height={50}/>
+        <span>Fudo</span>
+      </div>
+
+    </div>
   )
 }
 
