cabdaaed56863370753671c43609dc39a2acf625
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-15 21:38:18 +0100

    update

--- pages/pizza/[slug].js
+++ pages/pizza/[slug].js
@@ -1,36 +1,62 @@
+
+import Image from "next/image";
 import Layout from "../../components/Layout";
-import { client } from "../../lib/client";
+import { client, urlFor } from "../../lib/client";
+import css from "../../styles/Pizza.module.css"
 
 export default function Pizza({pizza}) {
-    console.log(pizza)
+    // console.log(pizza)
+    const src = urlFor(pizza.image).url()
     return(
         <Layout>
+            <div className={css.container}>
+                <div className={css.imageWrapper}>
+                    <Image
+                    loader={()=>src} 
+                    src={src}
+                    alt=""
+                    layout="fill"
+                    objectFit="cover"
+                    unoptimized
+                    />
+                </div>
+            </div>
 
-            Pizza page detail
+            {/* right side */}
+            <div className={css.size}>
+                <span>Size</span>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+            </div>
+
         </Layout>
-    )
+    );
 };
 
 export async function getStaticPaths(){
     const paths = await client.fetch(
         `*[_type=="pizza" && defined(slug.current)][].slug.current`
-    )
+    );
 
     return{
         paths: paths.map((slug)=>({params:{slug}})),
         fallback:'blocking',
-    }
+    };
 }
 
 export async function getStaticProps(context){
-    const{slug=""} = context.params
+    const{slug=""} = context.params;
     const pizza = await client.fetch(
-        `*[_type=="pizza" && slug.current=='${slug}'[0]]`
-    )
+        `*[_type=="pizza" && slug.current=='${slug}'][0]`
+    );
     return{
         props:{
             pizza,
         },
         
-    }
+    };
 }
