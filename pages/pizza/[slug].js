79b95bfde988a0729b1b48a8a5e72c4f8cee39ef
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-15 14:08:48 +0100

    update

--- pages/pizza/[slug].js
+++ pages/pizza/[slug].js
@@ -2,6 +2,7 @@ import Layout from "../../components/Layout";
 import { client } from "../../lib/client";
 
 export default function Pizza({pizza}) {
+    console.log(pizza)
     return(
         <Layout>
 
@@ -16,6 +17,20 @@ export async function getStaticPaths(){
     )
 
     return{
-        paths: paths.map((slug)=> ({params:{slug}}))
+        paths: paths.map((slug)=> ({params:{slug}})),
+        fallback:'blocking',
+    }
+}
+
+export async function getStaticProps(context){
+    const{slug=""} = context.params
+    const pizza = await client.fetch(
+        `*[_type=="pizza" && slug.current==${slug}[0]]`
+    )
+    return{
+        props:{
+            pizza,
+        },
+        
     }
 }
