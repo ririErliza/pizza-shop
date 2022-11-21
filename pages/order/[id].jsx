08c4c68f8353b7dc434b0958b6e14117c77a11c4
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-21 21:43:35 +0100

    update

---
+++ pages/order/[id].jsx
@@ -0,0 +1,52 @@
+import {client} from '../../lib/client'
+import Layout from '../../components/Layout'
+import css from '../../styles/Order.module.css'
+
+export const getServerSideProps = async({params})=>{
+    const query =`*[_type =='order' && _id =='${params.id}']`
+    const order = await client.fetch(query);
+
+    return{
+        props: {
+            order: order[0]
+        }
+    }
+};
+
+export default function Orders({order}) {
+    return(
+        <Layout>
+            <div className={css.container}>
+                <span className={css.heading}>
+                    Order in process
+                </span>
+                <div className={css.details}>
+                    <div>
+                        <span>Order ID</span>
+                        <span>{order._id}</span>
+                    </div>
+                    <div>
+                        <span>Customer Name</span>
+                        <span>{order.name}</span>
+                    </div>
+                    <div>
+                        <span>Phone</span>
+                        <span>{order.phone}</span>
+                    </div>
+                    <div>
+                        <span>Method</span>
+                        <span>
+                            {
+                                order.method === 0 ? 'Cash on delivery' : 'Online payment(Paid)'
+                            }
+                        </span>
+                    </div>
+                    <div>
+                        <span>Total</span>
+                        <span>$ {order.total}</span>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
